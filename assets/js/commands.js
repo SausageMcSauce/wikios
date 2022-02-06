eb67b369c80b5015dfdf673850290285f0d901ca
SausageMcSauce <69243388+SausageMcSauce@users.noreply.github.com>
2022-02-06 17:28:07 +0000

    finish about command

---
+++ assets/js/commands.js
@@ -0,0 +1,10 @@
+$('body').terminal({
+  about: function() {
+    this.echo("Created by SausageMcSauce / Jammum");
+    this.echo("Code at https://github.com/SausageMcSauce/wikios and under MIT License")
+    this.echo("Made using [[!;;;;https://terminal.jcubic.pl/]jQuery Terminal]")
+    this.echo("Based on this thing:")
+    this.echo("[[@;;;;./assets/img/mee.png]]")
+  }}, {
+    greetings: "Scratch Wiki OS - what even is this?"
+});
